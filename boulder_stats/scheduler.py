6042691306bdb9227258501ac41d3a57b1dd1d11
David Fischer <d_fi@gmx.de>
2020-10-28 19:44:57 +0100

    add: logging

--- boulder_stats/scheduler.py
+++ boulder_stats/scheduler.py
@@ -1,4 +1,5 @@
 """Schedule periodic data collection."""
+import logging
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -7,9 +8,13 @@ from .data_collector import DataCollector
 
 def start_data_collection():
     """Use scheduler to periodically collect data."""
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
     scheduler = BlockingScheduler()
     data_collector = DataCollector()
-    scheduler.add_job(data_collector.collect, trigger="cron", minute="15,45")
+    scheduler.add_job(data_collector.collect, trigger="cron", minute="15,23,24,45")
     scheduler.start()
 
 
